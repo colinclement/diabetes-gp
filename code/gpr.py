4593cd8a70a1ca075a7d8e49ec9cf64d80601ea7
Colin Clement <colin.clement@gmail.com>
2016-03-30 15:28:59 -0400

    renamed noised to var

--- code/gpr.py
+++ code/gpr.py
@@ -24,15 +24,18 @@ class GPR(object):
     def __init__(self, kernel, **kwargs):
         self.kernel = kernel
 
-    def fit(self, y_obs, x_obs, noise):
+    def fit(self, y_obs, x_obs, var):
         self.y_obs = y_obs
         self.x_obs = x_obs
         self.n = len(y_obs)
         self.K = self.kernel(x_obs, x_obs)
-        self.noise = noise
-        self._L = spl.cho_factor(self.K + noise*np.eye(self.n))
+        self.var = var
+        self._L = spl.cho_factor(self.K + var*np.eye(self.n))
         self.alpha = self._invKproduct(y_obs)
 
+    def _invKproduct(self, x):
+        return spl.cho_solve(self._L, x)
+    
     def transform(self, x_test):
         K_obs_test = self.kernel(self.x_obs, x_test)
         self.testmean = K_obs_test.T.dot(self.alpha)
@@ -41,40 +44,40 @@ class GPR(object):
                         K_obs_test.T.dot(self._invKproduct(K_obs_test)))
         return self.testmean, self.testvar
         
-    def fit_transform(self, y_obs, x_obs, noise, x_test):
-        self.fit(y_obs, x_obs, noise)
+    def fit_transform(self, y_obs, x_obs, var, x_test):
+        self.fit(y_obs, x_obs, var)
         return self.transform(x_test)
 
-    def logmarg(self, pnoise):
+    def logmarg(self, pvar):
         """Compute the log marginal likelihood of the fit.
 
         See Eq. 5.8 in Rasmussen & Williams (2006).
 
         input:
-            pnoise - array hyperparameters, whose last entry is the squared
-                    noise of the observations
+            pvar - array hyperparameters, whose last entry is the squared
+                    variance of the observations
             
         """
-        self.update_kernel_p(pnoise[:-1], pnoise[-1])
+        self.update_kernel_p(pvar[:-1], pvar[-1])
 
         lgmg = self.y_obs.dot(self.alpha) + \
-                2*np.sum(np.log(self._L[0].diagonal())) +\
-                self.n*np.log(2*np.pi)
+               2*np.sum(np.log(self._L[0].diagonal())) + \
+               self.n*np.log(2*np.pi)
         
         return -0.5*lgmg
 
-    def grad_logmarg(self, pnoise):
+    def grad_logmarg(self, pvar):
         """Compute the gradient of the log marginal likelihood wrt
-        hyperparameters and the squared noise.
+        hyperparameters and the variance.
 
         See Eq. 5.9 in Rasmussen & Williams (2006).
 
         input:
-            pnoise - array hyperparameters, whose last entry is the squared
-                    noise of the observations
+            pvar - array hyperparameters, whose last entry is the squared
+                    var of the observations
 
         """
-        self.update_kernel_p(pnoise[:-1], pnoise[-1])
+        self.update_kernel_p(pvar[:-1], pvar[-1])
 
         Kder = self.kernel.gradk(self.x_obs, self.x_obs)
 
@@ -85,11 +88,9 @@ class GPR(object):
 
         return 0.5*grd
 
-    def update_kernel_p(self, p, noise=None):
-        self.kernel.p = p
-        noise = noise or self.noise
-        self.fit(self.y_obs, self.x_obs, noise)
+    def update_kernel_p(self, p, var=None):
+        self.kernel.p[:] = p.copy()
+        var = var or self.var
+        self.fit(self.y_obs, self.x_obs, var)
 
-    def _invKproduct(self, x):
-        return spl.cho_solve(self._L, x)
 
